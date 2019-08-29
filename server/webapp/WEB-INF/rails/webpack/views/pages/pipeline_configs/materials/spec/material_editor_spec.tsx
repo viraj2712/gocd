a1fdbcea0199858612935573749eb018927fdf1e
Vrushali Waykole <vrushaliwaykole@gmail.com>
2019-08-29 16:46:59 +0530

    Updated material editor tests

--- server/webapp/WEB-INF/rails/webpack/views/pages/pipeline_configs/materials/spec/material_editor_spec.tsx
+++ server/webapp/WEB-INF/rails/webpack/views/pages/pipeline_configs/materials/spec/material_editor_spec.tsx
@@ -15,7 +15,7 @@
  */
 
 import m from "mithril";
-import {GitMaterialAttributes, Material, P4MaterialAttributes} from "models/new_pipeline_configs/materials";
+import {DependencyMaterialAttributes, GitMaterialAttributes, Material, P4MaterialAttributes} from "models/new_pipeline_configs/materials";
 import {TestHelper} from "views/pages/spec/test_helper";
 import {MaterialEditor} from "../material_editor";
 
@@ -37,23 +37,30 @@ describe("AddPipeline: Material Editor", () => {
     expect(helper.q("select")).toBeTruthy();
     expect(helper.q("label")).toBeTruthy();
     expect(helper.q("label").textContent).toBe("Material Type*");
-    expect(helper.textAll("option"))
-      .toEqual(["Git", "Mercurial", "Subversion", "Perforce", "Team Foundation Server", "Another Pipeline"]);
+    expect(helper.textAll("option")).toEqual(["Git", "Mercurial", "Subversion", "Perforce", "Team Foundation Server", "Another Pipeline"]);
   });
 
   it("Selecting a material updates the model type", () => {
     helper.onchange("select", "p4");
     expect(material.type()).toBe("p4");
     expect(material.attributes() instanceof P4MaterialAttributes).toBe(true);
-    expect(helper.byTestId("form-field-label-repository-url")).toBeFalsy();
-    expect(helper.byTestId("form-field-label-p4-view")).toBeTruthy();
-    expect(helper.byTestId("form-field-label-p4-view").textContent).toBe("P4 View*");
+    expect(helper.byTestId("form-field-label-repository-url")).not.toBeInDOM();
+    expect(helper.byTestId("form-field-label-p4-view")).toBeInDOM();
+    expect(helper.byTestId("form-field-label-p4-view")).toContainText("P4 View");
 
     helper.onchange("select", "git");
     expect(material.type()).toBe("git");
     expect(material.attributes() instanceof GitMaterialAttributes).toBe(true);
-    expect(helper.byTestId("form-field-label-p4-view")).toBeFalsy();
-    expect(helper.byTestId("form-field-label-repository-url")).toBeTruthy();
-    expect(helper.byTestId("form-field-label-repository-url").textContent).toBe("Repository URL*");
+    expect(helper.byTestId("form-field-label-p4-view")).not.toBeInDOM();
+    expect(helper.byTestId("form-field-label-repository-url")).toBeInDOM();
+    expect(helper.byTestId("form-field-label-repository-url")).toContainText("Repository URL");
+
+    helper.onchange("select", "dependency");
+    expect(material.type()).toBe("dependency");
+    expect(material.attributes() instanceof DependencyMaterialAttributes).toBe(true);
+    expect(helper.byTestId("form-field-label-repository-url")).not.toBeInDOM();
+    expect(helper.byTestId("form-field-label-upstream-pipeline")).toBeInDOM();
+    expect(helper.byTestId("form-field-label-upstream-pipeline")).toContainText("Upstream Pipeline");
+    expect(helper.byTestId("form-field-input-upstream-pipeline")).toBeInDOM();
   });
 });
